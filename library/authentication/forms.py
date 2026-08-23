ba7d6c396813290973bc6e85f93a907f8556ef63
Maksym Chumachenko <makchumachenko@gmail.com>
2026-08-23 19:34:00 +0300

    Working edit page

--- library/authentication/forms.py
+++ library/authentication/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
 from .models import CustomUser
 
@@ -11,27 +12,48 @@ class FormBase(forms.ModelForm):
             field.widget.attrs["class"] = "form-control"
 
 
-class LoginForm(FormBase):
+class LoginForm(forms.Form):
+    email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput)
 
-    class Meta:
-        model = CustomUser
-        fields = ["email", "password"]
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field in self.fields.values():
+            field.widget.attrs["class"] = "form-control"
 
 
-class RegisterForm(FormBase):
+class EditForm(FormBase):
     first_name = forms.CharField(required=True)
     last_name = forms.CharField(required=True)
+    
+    class Meta:
+        model = CustomUser
+        fields = ["email", "first_name", "last_name", "middle_name"]
+
+
+class RegisterForm(EditForm):
+    password = forms.CharField(widget=forms.PasswordInput)
 
     class Meta:
         model = CustomUser
         fields = ["email", "password", "first_name", "last_name", "middle_name"]
 
 
-class EditForm(RegisterForm):
+class PasswordForm(forms.Form):
     password = forms.CharField(widget=forms.PasswordInput)
-    password2 = forms.CharField(widget=forms.PasswordInput)
+    password2 = forms.CharField(widget=forms.PasswordInput, label="Repeat password")
 
-    class Meta:
-        model = CustomUser
-        fields = ["email", "first_name", "last_name", "middle_name"]
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field in self.fields.values():
+            field.widget.attrs["class"] = "form-control"
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        if cleaned_data.get("password") != cleaned_data.get("password2"):
+            raise ValidationError("Passwords don't match.")
+
+        return cleaned_data
--- library/authentication/views.py
+++ library/authentication/views.py
@@ -1,12 +1,11 @@
 from django.contrib import messages
 from django.contrib.auth import authenticate, get_user_model, login, logout
+from django.contrib.auth.decorators import login_required
 from django.core.exceptions import ValidationError
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import redirect, render
 
 from . import forms
-
-User = get_user_model()
 
 
 def login_view(request: HttpRequest) -> HttpResponse:
@@ -35,13 +34,14 @@ def register_view(request: HttpRequest) -> HttpResponse:
         form = forms.RegisterForm(request.POST)
 
         if form.is_valid():
-            cleaned = form.cleaned_data
+            cleaned = form.cleaned_data.copy()
             email = cleaned.pop("email")
             password = cleaned.pop("password")
 
             try:
-                user = User.objects.create_user(email, password, **cleaned)
-                login(request, user)
+                User = get_user_model()
+                new_user = User.objects.create_user(email, password, **cleaned)
+                login(request, new_user)
                 messages.success(request, f"Logged in as {email}")
                 return redirect("home")
             
@@ -56,12 +56,43 @@ def register_view(request: HttpRequest) -> HttpResponse:
     return render(request, "authentication/register.html", {"form": form})
 
 
+@login_required
 def logout_view(request: HttpRequest) -> HttpResponse:
-    email = request.user.email if request.user.is_authenticated else "user"
+    user = request.user
+    email = user.email if user.is_authenticated else "user"
     messages.success(request, f"Logged out of {email}")
     logout(request)
     return redirect("home")
 
 
+@login_required
 def edit_view(request: HttpRequest) -> HttpResponse:
-    pass
+    user = request.user
+
+    if request.method == "POST":
+        if request.POST.get("action") == "edit":
+            edit_form = forms.EditForm(request.POST, instance=user)
+            password_form = forms.PasswordForm()
+
+            if edit_form.is_valid():
+                edit_form.save()
+                return redirect("home")
+
+        elif request.POST.get("action") == "password":
+            edit_form = forms.EditForm(instance=user)
+            password_form = forms.PasswordForm(request.POST)
+
+            if password_form.is_valid():
+                user.set_password(password_form.cleaned_data["password"])
+                user.save()
+                login(request, user)
+                return redirect("home")
+
+    else:
+        edit_form = forms.EditForm(instance=user)
+        password_form = forms.PasswordForm()
+
+    return render(request, "authentication/edit.html", {
+            "edit_form": edit_form,
+            "password_form": password_form,
+        })
